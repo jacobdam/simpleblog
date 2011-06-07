7875095441823ca0dff20ff4a1851c97d4fb42da
Jacob Dam <jdam@digication.com>
2011-06-07 17:20:57 +0700

    Init rails project with backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleblog::Application.config.secret_token = '041071ff6ca9cb482c0acb917c01498d23ffcfe13898396d2d5af47827f164862e9f2d0449fe13264f58b1ec566995cad323c42fe80df037be6cf044ddc301e7'
